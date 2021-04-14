b17c7890396dade6d9c3a4c9d6e1a047faadecdb
Mayara <ads.nangela@gmail.com>
2021-04-14 17:05:30 -0300

    Incluido Get e Set

--- src/models/Atracao.java
+++ src/models/Atracao.java
@@ -13,4 +13,53 @@ public class Atracao {
 	String restricao;
 	CategoriaAtracao CategoriaAtracao;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdadeMinima() {
+		return idadeMinima;
+	}
+	public void setIdadeMinima(int idadeMinima) {
+		this.idadeMinima = idadeMinima;
+	}
+	public int getAlturaMinima() {
+		return alturaMinima;
+	}
+	public void setAlturaMinima(int alturaMinima) {
+		this.alturaMinima = alturaMinima;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getRestricao() {
+		return restricao;
+	}
+	public void setRestricao(String restricao) {
+		this.restricao = restricao;
+	}
+	public CategoriaAtracao getCategoriaAtracao() {
+		return CategoriaAtracao;
+	}
+	public void setCategoriaAtracao(CategoriaAtracao categoriaAtracao) {
+		CategoriaAtracao = categoriaAtracao;
+	}
+	
 }
