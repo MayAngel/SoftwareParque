e0f08460c4dcbcb3575b7af4dbb7108bb1891bb2
IasminBrey16 <80944417+IasminBrey16@users.noreply.github.com>
2021-04-21 17:46:17 -0300

    Update Visitante.java

--- src/models/Visitante.java
+++ src/models/Visitante.java
@@ -1,18 +1,85 @@
 package models;
-
 import java.sql.Date;
 
 public class Visitante {
 	
-	int id;
-	int idade;
-	int altura;
-	int peso;
-	String nome;
-	String cpf;
-	String endereco;
-	String telefone;
-	String email;
+	public Visitante() {
+		this.criadoEm = new Date();
+	}
+	
+	private int id;
+	private int idade;
+	private int altura;
+	private int peso;
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String telefone;
+	private String email;
+	private Date criadoEm;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCriadoEm() {
+		return criadoEm;
+	}
+	public void setCriadoEm(Date criadoEm) {
+		this.criadoEm = criadoEm;
+	}
+	
 	
 
 }
