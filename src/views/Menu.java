167939013285ae773ac484c13ef829a9b938a7d5
uesleyfucks <55562529+uesleyfucks@users.noreply.github.com>
2021-04-24 22:23:48 -0300

    Principal e Menu - Mayara

---
+++ src/views/Menu.java
@@ -0,0 +1,20 @@
+package views;
+
+public class Menu {
+	public void exibirMenu(){
+		System.out.println("\n");
+		System.out.println("+--------------------------------------+");
+		System.out.println("|                 MENU                 |");
+		System.out.println("+--------------------------------------+");
+		System.out.println(
+			"|  1 --Cadastro de Visitante--         |\n" +
+			"|  2 --Cadastro do Parque--            |\n" +
+			"|  3 --Cadastro de Atração--           |\n" +
+			"|  4 --Venda de Ingresso--	       |\n" +
+			"|  5 --Imprimir ingresso--   	       |\n" +
+			"|  0 --sair--                          |"
+		);
+		System.out.println("+--------------------------------------+\n\n");
+	}
+	
+}
--- src/views/Principal.java
+++ src/views/Principal.java
@@ -1,12 +1,67 @@
 package views;
 
-
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		
+		int opcao;
+	 Scanner sc = new Scanner(System.in);
+	 Menu menu = new Menu();
+	 do {
+		menu.exibirMenu();		 
+		System.out.println(" Digite a opção desejada: ");
+		opcao = sc.nextInt();		
+	
+		switch (opcao) {
+	//CadastrarVisitante Iasmin
+		case 1: 
+	//		CadastrarVisitante cadastrarVisitante = new CadastrarVisitante();
+	//		cadastrarVisitante.renderizar();			
+			break;
+	//CadastrarParque Wesley 	
+		case 2: 
+			CadastrarParque cadastrarParque = new CadastrarParque();
+			cadastrarParque.renderizar();			
+			break;
+			
+	//CadastroAtracao Mayara		
+		case 3: 
+			CadastrarAtracao cadastrarAtracao = new CadastrarAtracao();
+			cadastrarAtracao.renderizar();			
+			break;
+			
+	//VendaIngresso A Definir quem vai fazer 
+		case 4: 
+	//		CadastrarParque cadastrarParque = new CadastrarParque();
+	//		cadastrarParque.renderizar();			
+			break;
+			
+	//ImprimirIngresso A Definir quem vai fazer 
+		case 5: 
+	//		CadastrarParque cadastrarParque = new CadastrarParque();
+	//		cadastrarParque.renderizar();			
+			break;		
+		default:
+			System.out.println(opcao);		
+		break; 
+		}
+	} while (opcao!=0);
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 	}
+	
+	
 
 }
