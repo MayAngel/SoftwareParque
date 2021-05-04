65514322359f7df58cbb284c63918db9a8a5f661
Mayara <ads.nangela@gmail.com>
2021-05-04 08:40:34 -0300

    Alterado nome para Impressão do Ingresso

--- src/views/ImprimirIngresso.java
+++ src/views/ImprimirIngresso.java
@@ -6,7 +6,7 @@ import models.Ingresso;
 public class ImprimirIngresso {
 	
 	public static void renderizar() {
-		System.out.println("\n--- INGRESSOS ---  \n");
+		System.out.println("\n--- IMPRESSÃO DO INGRESSO ---  \n");
 		for(Ingresso vendaIngresso : VendaIngressoController.listar()) {
 			System.out.println(vendaIngresso);
 		}
